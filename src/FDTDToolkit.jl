5dc6671dc1bb415a8beb427f25cd11ae7dbe8060
Paul Shen <pxshen@alumni.stanford.edu>
2024-01-17 12:24:22 -0500

    a

--- src/FDTDToolkit.jl
+++ src/FDTDToolkit.jl
@@ -1,6 +1,5 @@
 module FDTDToolkit
-using CairoMakie
-using CairoMakie: Axis
+
 export recordsim, plotstep!
 include("main.jl")
 greet() = print("Hello World!")
--- src/main.jl
+++ src/main.jl
@@ -1,2 +1,4 @@
+using CairoMakie, UnPack
+using CairoMakie: Axis
 include("plot_recipes.jl")
-include("modes.jl")
+# include("modes.jl")
